266234c8e2935874a9ff89f28102369ac8990094
Jack Steele <jsteele883@gmail.com>
2017-06-28 22:45:19 -0700

    Initial commit sass and autoprefixer running

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // sass task
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          sourcemap: 'none',
+        },
+        files: {
+          'compiled/style-human.css': 'sass/style.scss',
+        }
+      },
+      dist: {
+        options: {
+          style: 'compressed',
+          sourcemap: 'none',
+        },
+        files: {
+          'compiled/style.css': 'sass/style.scss',
+        }
+      }
+    },
+    // autoprefixer task
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      // prefix all files
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'compiled/*.css',
+        dest: ''
+      }
+    },
+
+    // watch task
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass','autoprefixer']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask('default',['watch']);
+
+}
