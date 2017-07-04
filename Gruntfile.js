731ae2bc774c8c59385162d87a827d25df4e8a85
Jack Steele <jsteele883@gmail.com>
2017-07-03 18:29:46 -0700

    Remove files that weren't required

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,18 +12,9 @@ module.exports = function(grunt){
           sourcemap: 'none',
         },
         files: {
-          'compiled/style-human.css': 'sass/style.scss',
-        }
-      },
-      dist: {
-        options: {
-          style: 'compressed',
-          sourcemap: 'none',
-        },
-        files: {
           'compiled/style.css': 'sass/style.scss',
         }
-      }
+      },
     },
     // autoprefixer task
     autoprefixer: {
