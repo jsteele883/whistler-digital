8d204f5207406b9042f3b0cb7a18b719074eadc2
Jack Steele <jsteele883@gmail.com>
2017-09-04 21:30:32 -0700

    Adjusted header and footer styles

--- footer.php
+++ footer.php
@@ -15,15 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'whistlerdigital' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'whistlerdigital' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'whistlerdigital' ), 'whistlerdigital', '<a href="https://automattic.com/">Jack Steele</a>' );
-			?>
+			<p>&copy; Lonely Comets 2017</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
