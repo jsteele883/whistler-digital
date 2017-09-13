2d1d3c755c931d91067e6ab45467cbb541a54915
Jack Steele <jsteele883@gmail.com>
2017-09-12 20:47:21 -0700

    Make year date flexible

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<p>&copy; Lonely Comets 2017</p>
+			<p>&copy; Lonely Comets <?php echo date("Y"); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
