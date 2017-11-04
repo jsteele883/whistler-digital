cab0ddde2fb378ac167fe1daa52449d7f8150bab
Jack Steele <jsteele883@gmail.com>
2017-11-04 11:52:21 -0700

    setup for whistler digital

--- footer.php
+++ footer.php
@@ -15,7 +15,22 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<p>&copy; Lonely Comets <?php echo date("Y"); ?></p>
+			<p>&copy; Whistler Digital <?php echo date("Y"); ?></p>
+			<?php
+			// Make sure there is a social menu to display.
+			if ( has_nav_menu( 'social' ) ) { ?>
+			<nav class="social-menu">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'social',
+						'menu_class'     => 'social-links-menu',
+						'depth'          => 1,
+						'link_before'    => '<span class="social-links-menu--hidden-text">',
+						'link_after'     => '</span>' . whistlerdigital_get_svg( array( 'icon' => 'chain' ) ),
+					) );
+				?>
+			</nav><!-- .social-menu -->
+			<?php } ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -49,21 +49,6 @@
 					) );
 				?>
 			</nav><!-- #site-navigation -->
-			<?php
-			// Make sure there is a social menu to display.
-			if ( has_nav_menu( 'social' ) ) { ?>
-			<nav class="social-menu">
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'social',
-						'menu_class'     => 'social-links-menu',
-						'depth'          => 1,
-						'link_before'    => '<span class="social-links-menu--hidden-text">',
-						'link_after'     => '</span>' . whistlerdigital_get_svg( array( 'icon' => 'chain' ) ),
-					) );
-				?>
-			</nav><!-- .social-menu -->
-			<?php } ?>
 		</div>
 	</header><!-- #masthead -->
 
