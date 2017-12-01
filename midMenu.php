711e76069647557fe20358bd0de934f3b0fe0425
Jack Steele <jsteele883@gmail.com>
2017-11-30 20:34:59 -0800

    Moved content above header on homepage

---
+++ midMenu.php
@@ -0,0 +1,37 @@
+<div class="introduction" height="<?php echo esc_attr( get_custom_header()-> height); ?>">
+	<div class="l-row">
+		<div class="l-col-3">
+			<div class="masthead-menu masthead-menu__yellow">
+				<div class="masthead-menu__icon">
+					<?php echo whistlerdigital_get_svg( array( 'icon' => 'codepen', 'title' => __( 'code', 'textdomain' ) ) ); ?>
+				</div>
+				<div class="masthead-menu__text">
+					<h2>Web Development</h2>
+					<p>We will help you build the website of your dreams without blowing up your budget.</p>
+				</div>
+			</div>
+		</div>
+		<div class="l-col-3">
+			<div class="masthead-menu masthead-menu__orange">
+				<div class="masthead-menu__icon">
+					<?php echo whistlerdigital_get_svg( array( 'icon' => 'chain', 'title' => __( 'This is the title', 'textdomain' ) ) ); ?>
+				</div>
+				<div class="masthead-menu__text">
+					<h2>IT Consulting</h2>
+					<p>Let us build your IT platform for a worry-free experience, contact us for a quote!</p>
+				</div>
+			</div>
+		</div>
+		<div class="l-col-3">
+			<div class="masthead-menu masthead-menu__red">
+				<div class="masthead-menu__icon">
+					<?php echo whistlerdigital_get_svg( array( 'icon' => 'hashtag', 'title' => __( 'This is the title', 'textdomain' ) ) ); ?>
+				</div>
+				<div class="masthead-menu__text">
+					<h2>Marketing</h2>
+					<p>Maximise your potential revenue with the help of our email and social expertise</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- single.php
+++ single.php
@@ -29,7 +29,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
+get_midMenu();
 get_sidebar();
 get_footer();
